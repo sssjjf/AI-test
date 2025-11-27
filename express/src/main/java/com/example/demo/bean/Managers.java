5935ad1f40d1868785ad098d7a1fdf434120d4c2
sssjjf <73323159+sssjjf@users.noreply.github.com>
2025-11-27 14:24:25 +0800

    Delete express directory

--- express/src/main/java/com/example/demo/bean/Managers.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.bean;
-
-public class Managers {
-    public static String superManager ="superManager";
-    public static String bigManager ="bigManager";
-    public static String midManager ="midManager";
-    public static String commonUser ="commonUser";
-}
